025a3666ae12c44fcb5d51ec25ca137ba3429e7a
AlexeyShirokikh <shirokih-1997@mail.ru>
2021-05-19 01:36:07 +0300

    дефолтные рекомендации

--- recommendations_api/src/storage/elastic.py
+++ recommendations_api/src/storage/elastic.py
@@ -1,64 +1,27 @@
-from typing import List, Dict, Tuple, Optional
-from uuid import UUID
+from typing import List, Dict
 
 import backoff
 from elasticsearch import AsyncElasticsearch
 from elasticsearch.exceptions import ConnectionError, ConnectionTimeout
 
 from db import elastic
-from storage.abstract import Storage
 
 exceptions_list = (ConnectionError, ConnectionTimeout,)
 
 
-class ElasticStorage(Storage):
+class ElasticStorage:
 
     def __init__(self, es: AsyncElasticsearch):
         self.elastic = es
 
     @backoff.on_exception(backoff.expo, exceptions_list, max_tries=10)
-    async def get_by_ids(self, index: str, ids: List[UUID]) -> List[Dict]:
+    async def get_ordered_list(self, index_, size=10, field='imdb_rating', order='desc') -> List[Dict]:
         """
-        Получить объекты по списку ID
+        Получить список, упорядоченный по полю field
         """
-        doc_ids = [{'_id': _id} for _id in ids]
-        resp = await self.elastic.mget(index=index, body={'docs': doc_ids})
-        docs = [doc['_source'] for doc in resp['docs'] if doc['found']]
+        resp = await self.elastic.mget(index=index_, body={'sort': {field: {'order': order}}})
+        docs = [doc['_source']['id'] for doc in resp['docs'] if doc['found']][:size]
         return docs
-
-    @backoff.on_exception(backoff.expo, exceptions_list, max_tries=10)
-    async def search(
-            self,
-            index: str,
-            body: Optional[Dict] = None,
-            params: Optional[Dict] = None,
-    ) -> Tuple[int, List[UUID]]:
-        """
-        Поиск по объектам
-        """
-        if params is None:
-            params = {}
-        params.update({"_source": False, })
-        docs = await self.elastic.search(index=index, body=body, params=params)
-        ids = [UUID(doc['_id']) for doc in docs['hits']['hits']]
-        total = docs['hits']['total']['value']
-        return (total, ids)
-
-    @backoff.on_exception(backoff.expo, exceptions_list, max_tries=10)
-    async def msearch(
-            self,
-            index: str,
-            body: Optional[List[Dict]] = None,
-            params: Optional[Dict] = None,
-    ) -> List[Optional[List[UUID]]]:
-        """
-        Несколько поисковых запросов в одном
-        """
-        response = await self.elastic.msearch(index=index, body=body, params=params)
-        result = []
-        for docs in response['responses']:
-            result.append([UUID(doc['_id']) for doc in docs['hits']['hits']])
-        return result
 
 
 def get_elastic_storage() -> ElasticStorage:
